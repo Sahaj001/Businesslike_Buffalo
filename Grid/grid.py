73088136833b38eeb5ed22d5ba0a6461b9c8b0d8
Ibrahim2750mi <74553450+Ibrahim2750mi@users.noreply.github.com>
2021-07-10 19:39:58 +0530

    Update grid.py

--- Grid/grid.py
+++ Grid/grid.py
@@ -1,13 +1,5 @@
-def create_line(width_of_line: int) -> str:
-    """Returns a line.
-
-    :param width_of_line: The width of the line it wants to create.
-    :return: A string having length width_of_line
-    """
-    line = ""
-    for _ in range(width_of_line):
-        line += " "
-    return line
+import numpy as np
+import time
 
 
 class Grid:
@@ -22,32 +14,27 @@ class Grid:
         :param width: The width of the grid
         :param height: The height of the grid
         """
-        self.width = width
-        self.height = height
-        grid = []
-        for _ in range(height):
-            line = create_line(width_of_line=width)
-            grid.append(line)
+        self.width = width - 1
+        self.height = height - 1
+        grid = np.full((height, width), " ")
         self.grid = grid
 
     def __repr__(self) -> str:
-        return '\n'.join(self.grid)
+        self.grid = np.c_[self.grid, np.full((self.height+1, 1), "\n")]
+        return ''.join(self.grid.ravel())
 
-    def insert(self, pos_x: int, pos_y: int, object_to_be_rendered: str) -> None:
+    def insert(self, pos_x: int, pos_y: int, object_to_be_rendered: list, object_height: int = 1,
+               object_width: int = 1) -> None:
         """Inserts a object to the specified point.
 
         :param pos_x: The x axis point of the grid where object is to be rendered.
         :param pos_y: The y axis point of the grid where object is to be rendered.
         :param object_to_be_rendered: The object you want to render.
+        :param object_width: The object's width.
+        :param object_height: The object's height.
         :return: Nothing.
         """
-        line = self.grid[pos_y]
-        # Converts line to a list.
-        mutated_line = list(map(lambda line_elem: line_elem, line))
-        # Adds the object to the desired place.
-        mutated_line[pos_x] = object_to_be_rendered
-        # Converts the line to a string.
-        self.grid[pos_y] = ''.join(mutated_line)
+        self.grid[pos_y:pos_y + object_height, pos_x:pos_x+object_width] = np.array(object_to_be_rendered)
 
     def check(self, pos_x: int, pos_y: int) -> bool:
         """Checks whether the user specified point is blank or not
@@ -56,12 +43,8 @@ class Grid:
         :param pos_y: The y axis point of the grid where the user want to check.
         :return: A boolean object.
         """
-        line = self.grid[pos_y]
-        # Converts line to a list.
-        mutated_line = list(map(lambda line_elem: line_elem, line))
-        # Gets the point where the user want to check.
-        user_point = mutated_line[pos_x]
 
+        user_point = self.grid[pos_y:pos_y + 1, pos_x:pos_x + 1]
         if user_point == " ":
             # Checks if the point is empty, if yes it returns True if no then it returns False.
             return True
@@ -70,17 +53,18 @@ class Grid:
 
 
 if __name__ == "__main__":
-    plane = Grid()
-    plane.insert(0, 0, "┌")
-    plane.insert(0, 99, "└")
-    for plane_x in range(1, 99):
-        plane.insert(plane_x, 0, "─")
-        plane.insert(plane_x, 99, "─")
-    plane.insert(99, 0, "┐")
-    plane.insert(99, 99, "┘")
+    start = time.perf_counter()
+    plane = Grid(80, 24)
+    plane.insert(0, 0, ["┌"])
+    plane.insert(0, 23, ["└"])
+    for plane_x in range(1, 79):
+        plane.insert(plane_x, 0, ["─"])
+        plane.insert(plane_x, 23, ["─"])
+    plane.insert(79, 0, ["┐"])
+    plane.insert(79, 23, ["┘"])
 
-    for plane_y in range(1, 99):
-        plane.insert(0, plane_y, "│")
-        plane.insert(99, plane_y, "│")
-
+    for plane_y in range(1, 23):
+        plane.insert(0, plane_y, ["│"])
+        plane.insert(79, plane_y, ["│"])
+    plane.insert(1, 1, ["w", 'w', 'w', 'w', 'w', 'w', 'w', 'w'], object_width=8)
     print(plane)
