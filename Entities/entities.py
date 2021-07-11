9ab24d21e4ca11455cb02f9a0cfa2846657240b4
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2021-07-11 11:24:20 +0800

    Fixed whitespace bug and corrected linting

--- Entities/entities.py
+++ Entities/entities.py
@@ -1,41 +1,77 @@
 import os
+
 import numpy as np
 
 ASCII_FOLDER = "assets/ascii"
 
 
 class Entity:
+    """A base class for creating entities from ascii files.
+
+    Defines attributes x, y, width, height, which_screen as well as method render()
+    that can be accessed for rendering.
+    """
+
     def __init__(self, ascii_file: str, x: int, y: int, which_screen: int) -> None:
-        """Initialises Entity object based on ASCII stored in file"""
+        """Initialises Entity object based on ASCII stored in file.
+
+        :param ascii_file: The filename containing the ascii art in the ascii folder.
+        :param x: The x position of the entity.
+        :param y: The y position of the entity.
+        :param which_screen: An integer noting which screen the entity is on.
+        """
         self.x = x
         self.y = y
         self.which_screen = which_screen
 
         # Processing ascii file
         with open(os.path.join(ASCII_FOLDER, ascii_file), "r", encoding="utf8") as file:
-            ascii_lst = file.read().strip().splitlines()
+            ascii_lst = file.read().rstrip().splitlines()
 
         self.height = len(ascii_lst)  # Finding the height of the ascii art
-        self.width = max([len(line) for line in ascii_lst])  # Finding the width of the ascii art
+        self.width = max(
+            [len(line) for line in ascii_lst]
+        )  # Finding the width of the ascii art
 
         # Padding out the lines in case needed white space isn't in ascii file
-        ascii_lst = [line + (self.width-len(line))*" " for line in ascii_lst]
+        ascii_lst = [line + (self.width - len(line)) * " " for line in ascii_lst]
 
         # Storing the rendered table
         self.rendered_table = np.array([list(line) for line in ascii_lst])
 
     def render(self) -> np.ndarray:
-        """A method that returns the rendered table"""
+        """A method that returns the rendered table as a 2D numpy array."""
         return self.rendered_table
 
 
 class Tree(Entity):
-    def __init__(self, x: int, y: int, which_screen: int):
+    """A derived Entity class creating a tree object."""
+
+    def __init__(self, x: int, y: int, which_screen: int) -> None:
+        """Creates a tree based on the tree ascii file in the ascii folder.
+
+        :param x: The x position of the entity.
+        :param y: The y position of the entity.
+        :param which_screen: An integer noting which screen the entity is on.
+        """
         ascii_file = "tree.txt"
         super().__init__(ascii_file=ascii_file, x=x, y=y, which_screen=which_screen)
 
 
 class Bar(Entity):
-    def __init__(self, x: int, y: int, which_screen: int):
+    """A derived Entity class creating a bar object."""
+
+    def __init__(self, x: int, y: int, which_screen: int) -> None:
+        """Creates a bar based on the bar ascii file in the ascii folder.
+
+        :param x: The x position of the entity.
+        :param y: The y position of the entity.
+        :param which_screen: An integer noting which screen the entity is on.
+        """
         ascii_file = "bar.txt"
         super().__init__(ascii_file=ascii_file, x=x, y=y, which_screen=which_screen)
+
+
+if __name__ == "__main__":
+    tree = Tree(1, 2, 1)
+    print(tree.render())
