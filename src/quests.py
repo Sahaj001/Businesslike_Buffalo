6b900069c292b8d13cc3e8717aa280be1dbb2561
Ravi <jravi248@gmail.com>
2021-07-16 20:49:43 +0530

    Replaced txt files with json with formatting, and some other minor changes

--- src/quests.py
+++ src/quests.py
@@ -12,12 +12,12 @@ class Quest:
         self.mcq = False
         self.progression = (0, 0)
 
-        text_variables = {1: self.quest_1_messages, 2: self.quest_2_messages,
+        self.text_variables = {1: self.quest_1_messages, 2: self.quest_2_messages,
                           3: self.quest_3_messages, 4: self.quest_4_messages}
 
         for script_value in range(1, 5):
             with open(f"./script/quest_{script_value}_messages.txt") as f:
-                text_variables[script_value] = json.load(f)
+                self.text_variables[script_value] = json.load(f)
 
     def reset(self):
         self.progression = (0, 0)
