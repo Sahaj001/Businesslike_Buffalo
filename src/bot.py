0d5c12ec8eb11d802c28880a33978f0ef5804b86
Sahaj Pratap Singh <sahajsingh0022@gmail.com>
2021-07-17 21:54:35 +0530

    fixed

--- src/bot.py
+++ src/bot.py
@@ -20,7 +20,7 @@ class Bot:
         self.path = "../assets/ascii/text/"
         self.ascii_bot, self.height, self.width = self.read_text(botfile)
 
-    def read_text(self, filename: str) -> (np.ndarray, int, int):
+    def read_text(self, filename: str):
         """Reads bot.txt.
 
         :param filename: The filename it needs to read.
@@ -82,3 +82,11 @@ class StartScreen:
         :param bot: Bot class
         """
         self.table[bot.x - 1:bot.x + bot.height + 1, bot.y - 1:bot.y + bot.width + 1] = " "
+
+    def print_st(self, table):
+        st = ""
+        for i in table:
+            for j in i:
+                st += j
+            st += '\n'
+        return st
