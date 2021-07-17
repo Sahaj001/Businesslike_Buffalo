4586a66d018a34041f86e82bf0d836b6ee06f217
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2021-07-17 15:18:23 +0800

    Changed encoding to utf8 (error on Windows otherwise)

--- src/first_screen.py
+++ src/first_screen.py
@@ -1,17 +1,21 @@
-import bot
+import time
+
 # import play_sound
 import simpleaudio as sa
 from prompt_toolkit.application import Application
 from prompt_toolkit.application.current import get_app
-from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.document import Document
-from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
 from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.key_binding.bindings.focus import (
+    focus_next, focus_previous
+)
 from prompt_toolkit.key_binding.key_processor import KeyPressEvent
 from prompt_toolkit.layout import HSplit, Layout, VSplit, Window, WindowAlign
+from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.styles import Style
 from prompt_toolkit.widgets import Box, Button, Frame, Label, TextArea
-import time
+
+import bot
 
 
 class GameScreen:
@@ -248,7 +252,7 @@ class GameScreen:
         Calls the run function from Application to lauch the window
         """
 
-        with open(f"{self.path}bot2.txt", 'r') as file:
+        with open(f"{self.path}bot2.txt", 'r', encoding="utf-8") as file:
             bt2 = file.read()
         body = Window(
             FormattedTextControl(bt2),
